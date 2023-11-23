1bdaf587ae88384468aa2c9120873bb96542732e
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-11-23 16:07:22 +0530

    Project initiation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="relative flex min-h-screen flex-col bg-[#f1f1f1]">
+            <div className="">nav</div>
+            <div className="flex flex-1 flex-col">content</div>
+            <div className="bg-white">hello there</div>
+            <div className="mt-auto">footer</div>
+        </div>
+    )
+}
+
+export default App
