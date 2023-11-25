85fd548fe383eea59e173b13a90f93bba2a9ba60
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-11-26 00:15:20 +0530

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Layout } from './components'
+import { ErrorBoundary } from 'react-error-boundary'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages'
 
 const App = () => {
     return (
         <div className="relative flex min-h-screen flex-col bg-[#f1f1f1]">
-            <div className="">nav</div>
-            <div className="flex flex-1 flex-col">content</div>
-            <div className="bg-white">hello there</div>
-            <div className="mt-auto">footer</div>
+            <Suspense>
+                <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                        </Routes>
+                    </Layout>
+                </ErrorBoundary>
+            </Suspense>
         </div>
     )
 }
