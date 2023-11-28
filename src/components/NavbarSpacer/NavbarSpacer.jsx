c6d4a612bfe191bdaf656058bae27266bb17de8a
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-11-28 22:34:29 +0530

    Add NavbarSpacer component to components

---
+++ src/components/NavbarSpacer/NavbarSpacer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarSpacer = () => {
+    return (
+        <>
+            <div className="h-14 sm:h-16" />
+        </>
+    )
+}
+
+export default NavbarSpacer
