4907107916309e5066b214339b99a01547e30b9c
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-11-29 00:35:19 +0530

    Update NavbarSpacer component

--- src/components/NavbarSpacer/NavbarSpacer.jsx
+++ src/components/NavbarSpacer/NavbarSpacer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavbarSpacer = () => {
     return (
         <>
-            <div className="h-14 sm:h-16" />
+            <div className="h-14" />
         </>
     )
 }
