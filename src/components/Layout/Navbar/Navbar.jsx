719b53e3183a69aad08558988102fadbce8c91a9
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-02 14:38:06 +0530

    Update navbar component

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -18,26 +18,22 @@ const Navbar = ({ logoUrl, logoOnly = false, brandName, links }) => {
     return (
         <>
             <header
-                className="fixed inset-x-0 top-0 z-[9999] flex h-14 w-full items-center border-b border-slate-800/20 bg-white p-3 transition-all duration-300  sm:px-4 sm:py-3"
+                className="fixed inset-x-0 top-0 z-[9999] flex h-14 w-full items-center bg-slate-800 p-3 transition-all duration-300  sm:px-4 sm:py-3"
                 ref={navbarRef}
             >
-                <div className="text-xl font-medium text-slate-800">
+                <div className="text-xl font-medium text-[#fefefe]">
                     {logoUrl && <img src={logoUrl} alt={brandName} />}
                     {!logoOnly && brandName && <>{brandName}</>}
                 </div>
                 <button
                     type="button"
                     onClick={handleToggle}
-                    className={`z-[99999] ml-auto flex items-center justify-center rounded-full border border-slate-800/10 p-2 transition-all duration-0 lg:hidden ${
-                        isToggled
-                            ? 'bg-indigo-500 text-white'
-                            : 'text-slate-800'
-                    }`}
+                    className={`z-[99999] ml-auto flex items-center justify-center rounded-full border border-slate-800/10 bg-indigo-500 p-2 text-[#fefefe] transition-all duration-0 lg:hidden`}
                 >
                     {isToggled ? <FaX /> : <FaBars />}
                 </button>
                 <nav
-                    className={`fixed inset-0 z-[9999] flex h-screen w-full flex-col items-center justify-center bg-slate-800/70 backdrop-blur-sm transition-all duration-300 ease-in-out lg:relative lg:inset-auto lg:ml-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:bg-transparent${
+                    className={`fixed inset-0 z-[9999] flex h-screen w-full flex-col items-center justify-center bg-slate-800/75 backdrop-blur-md transition-all duration-300 ease-in-out lg:relative lg:inset-auto lg:ml-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:bg-transparent${
                         isToggled ? ' translate-x-0' : ' translate-x-full'
                     }`}
                 >
