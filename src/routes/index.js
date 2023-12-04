96b60a13611c6e0a001ababd39d7c557bcac55c4
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-04 18:22:07 +0530

    Update routes index.js

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { Home } from '../pages'
+
+const Routes = [
+    {
+        id: 1,
+        name: 'Home',
+        path: '/',
+        main: true,
+        icon: false,
+        component: Home,
+        children: [],
+    },
+]
+
+export default Routes
