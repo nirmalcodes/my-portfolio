e651185397204e7d49a88186d798731a3b44e2ec
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-07 02:51:18 +0530

    Update home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,20 @@
 import React from 'react'
 import { images } from '../../utils/constants'
+import { HeroContainer } from '../../containers'
+
 const Home = () => {
     return (
         <>
             <div className="">
-                <section
-                    className="flex h-screen flex-col items-center justify-center bg-red-300"
-                    id={'hero'}
-                >
-                    hero
+                <HeroContainer />
+                <section className="section" id={'about'}>
+                    <div className="container">about</div>
                 </section>
-                <section
-                    className="flex h-screen flex-col items-center justify-center bg-blue-300"
-                    id={'about'}
-                >
-                    about
+                <section className="section" id={'projects'}>
+                    <div className="container">projects</div>
                 </section>
-                <section
-                    className="flex h-screen flex-col items-center justify-center bg-green-300"
-                    id={'projects'}
-                >
-                    projects
-                </section>
-                <section
-                    className="flex h-screen flex-col items-center justify-center bg-teal-300"
-                    id={'contact'}
-                >
-                    contact
+                <section className="section" id={'contact'}>
+                    <div className="container">contact</div>
                 </section>
             </div>
         </>
