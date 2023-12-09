fed794c7f77eb0fa04ff724b4f31b94816db640e
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-10 02:59:55 +0530

    Update AboutContainer

--- src/containers/AboutContainer/AboutContainer.jsx
+++ src/containers/AboutContainer/AboutContainer.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const AboutContainer = () => {
     return (
         <>
-            <section className="section" id={'about'}>
+            <section className="section p-4" id={'about'}>
                 <div className="container flex min-h-screen flex-col items-center justify-center md:block">
-                    <h3 className="relative mb-7 text-[64px] font-bold leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[50%] lg:-translate-y-[50%] lg:text-[160px] xl:text-[200px]">
+                    <h3 className="relative mb-7 text-[64px] font-bold leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[160px] xl:text-[200px]">
                         About Me
                     </h3>
-                    <p className="text-2xl text-slate-800 md:absolute md:left-[15%] md:top-[55%] md:-translate-y-[55%] lg:top-[72%] lg:-translate-y-[72%] lg:text-[38px] lg:leading-[42px]">
+                    <p className="text-2xl text-slate-800 md:absolute md:left-[15%] md:top-[55%] md:-translate-y-[55%] lg:top-[65%] lg:-translate-y-[65%] lg:text-[38px] lg:leading-[42px]">
                         Turning designs into interactive wonders. Crafting
                         clean, optimized, and reusable code is my forte. And a
                         rapid learner passionate about delivering seamless user
