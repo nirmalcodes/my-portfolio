93939e6dfbfe2938081968528ede8350e3318c13
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-16 01:24:11 +0530

    Update ProjectCard component

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -17,13 +17,15 @@ const ProjectCard = ({ projectData }) => {
         <>
             <div className="mb-4 flex flex-row flex-wrap  gap-x-8 gap-y-2 rounded-[8px] bg-white p-3 shadow-md md:p-4 md:even:flex-row-reverse">
                 {/* project previwe image */}
-                <div className="max-h-[320px] w-full overflow-hidden rounded-[6px] md:max-h-none md:w-[320px] lg:w-[360px] xl:max-h-[360px] xl:w-1/2">
+                <div className="relative flex max-h-[320px] w-full flex-col overflow-hidden rounded-[6px] md:max-h-none md:w-[320px] lg:w-[360px] xl:max-h-[360px] xl:w-1/2">
+                    <div className="absolute inset-0 z-0 min-h-[180px] flex-1 animate-pulse bg-slate-200" />
                     <img
+                        // hidden
                         src={imgURL}
                         alt={title}
                         loading="lazy"
                         width={400}
-                        className="h-full w-full object-cover"
+                        className="z-[1] h-full w-full object-cover"
                     />
                 </div>
 
