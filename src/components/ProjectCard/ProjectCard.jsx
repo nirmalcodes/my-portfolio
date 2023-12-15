c44ee0ae0a098c15a9303346345648f02653c0c6
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-16 00:30:23 +0530

    Add ProjectCard component to components

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaGithub, FaLink } from 'react-icons/fa'
+
+const ProjectCard = ({ projectData }) => {
+    const {
+        imgURL,
+        title,
+        createdAt,
+        description,
+        techStack = [],
+        demoLink,
+        githubLink,
+    } = projectData
+
+    return (
+        <>
+            <div className="mb-4 flex flex-row flex-wrap  gap-x-8 gap-y-2 rounded-[8px] bg-white p-3 shadow-md md:p-4 md:even:flex-row-reverse">
+                {/* project previwe image */}
+                <div className="w-full overflow-hidden rounded-[6px] sm:max-h-[360px] md:max-h-none md:w-[320px] lg:w-[360px] xl:max-h-[360px] xl:w-1/2">
+                    <img
+                        src={imgURL}
+                        alt={title}
+                        loading="lazy"
+                        width={400}
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+
+                {/* project details */}
+                <div className="flex min-h-full flex-1 flex-col bg-blue-200/0 py-4 lg:pr-5 xl:pr-[200px]">
+                    {/* project title */}
+                    <h5 className="text-lg font-bold text-slate-800">
+                        {title ? title : 'Title'}
+                    </h5>
+                    {/* project created date */}
+                    <span className="mb-3 text-sm font-bold text-slate-500">
+                        {createdAt ? createdAt : 'DD/MM/YYYY'}
+                    </span>
+
+                    {/* project description */}
+                    <p className="mb-3 font-medium">
+                        {description ? description : 'Description'}
+                    </p>
+
+                    {/* project tech stack */}
+                    {techStack && techStack.length !== 0 && (
+                        <ul className="mb-6 flex items-center gap-2 md:mb-10">
+                            {techStack.map((tech, index) => (
+                                <li key={`tech-${index}`}>
+                                    <img
+                                        src={`https://skillicons.dev/icons?i=${tech}`}
+                                        alt={tech}
+                                        loading="lazy"
+                                        width={32}
+                                        className="transition-all duration-300 ease-in"
+                                    />
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+
+                    {/* project buttons */}
+                    {demoLink ||
+                        (githubLink && (
+                            <ul className="flex items-center gap-5">
+                                {demoLink && (
+                                    <li>
+                                        <Link
+                                            to={demoLink}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="flex cursor-pointer items-center rounded-md bg-white px-4 py-2 text-slate-800 shadow-md duration-300 ease-in hover:scale-110"
+                                        >
+                                            <FaLink className="mr-2" /> Demo
+                                        </Link>
+                                    </li>
+                                )}
+                                {githubLink && (
+                                    <li>
+                                        <Link
+                                            to={githubLink}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="flex cursor-pointer items-center rounded-md bg-white px-4 py-2 text-slate-800 shadow-md duration-300 ease-in hover:scale-110"
+                                        >
+                                            <FaGithub className="mr-2" /> Code
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectCard
