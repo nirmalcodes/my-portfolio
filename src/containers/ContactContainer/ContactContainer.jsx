ca4506d28f361b40427b173775bc49be1d60a9cc
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 01:19:14 +0530

    Update ContactContainer

--- src/containers/ContactContainer/ContactContainer.jsx
+++ src/containers/ContactContainer/ContactContainer.jsx
@@ -64,7 +64,7 @@ const ContactContainer = () => {
             <a href="http://" target="_blank" rel="noopener noreferrer"></a>
             <section className="section" id={'contact'}>
                 <div className="container flex min-h-screen flex-col items-center justify-center md:block md:p-4">
-                    <h3 className="relative mb-7 text-[64px] font-bold uppercase leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[160px] xl:text-[200px]">
+                    <h3 className="relative mb-7 text-[64px] font-bold uppercase leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[156px] xl:text-[200px]">
                         Contact
                     </h3>
                     <div className="text-2xl text-slate-800 md:absolute md:left-[10%] md:top-[50%] md:-translate-y-[50%] lg:top-[55%] lg:-translate-y-[55%] lg:text-[38px] lg:leading-[42px]">
