42711e527c26c61169e1115b31f65b3b6692d41e
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 04:31:24 +0530

    Update ContactContainer

--- src/containers/ContactContainer/ContactContainer.jsx
+++ src/containers/ContactContainer/ContactContainer.jsx
@@ -11,6 +11,7 @@ import {
     FaLink,
 } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
+import { SlideInAnimation } from '../../components'
 
 const iconFinder = (iconName) => {
     switch (iconName) {
@@ -35,38 +36,42 @@ const iconFinder = (iconName) => {
     }
 }
 
-const ContactContainer = () => {
-    const contacts = [
-        {
-            name: 'email',
-            url: 'mailto:srnfernando102@gmail.com',
-            type: 'normal',
-        },
-        {
-            name: 'instagram',
-            url: 'https://www.instagram.com/',
-            type: 'new',
-        },
-        {
-            name: 'twitter',
-            url: 'https://www.twitter.com/',
-            type: 'new',
-        },
-        {
-            name: 'linkedin',
-            url: 'https://www.linkedin.com/',
-            type: 'new',
-        },
-    ]
+const contacts = [
+    {
+        name: 'email',
+        url: 'mailto:srnfernando102@gmail.com',
+        type: 'normal',
+    },
+    {
+        name: 'instagram',
+        url: 'https://www.instagram.com/',
+        type: 'new',
+    },
+    {
+        name: 'twitter',
+        url: 'https://www.twitter.com/',
+        type: 'new',
+    },
+    {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/',
+        type: 'new',
+    },
+]
 
+const ContactContainer = () => {
     return (
         <>
-            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
             <section className="section" id={'contact'}>
                 <div className="container flex min-h-screen flex-col items-center justify-center md:block md:p-4">
-                    <h3 className="relative mb-7 text-[64px] font-bold uppercase leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[156px] xl:text-[200px]">
+                    <SlideInAnimation
+                        component="h3"
+                        className={
+                            'relative mb-7 text-[64px] font-bold uppercase leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[156px] xl:text-[200px]'
+                        }
+                    >
                         Contact
-                    </h3>
+                    </SlideInAnimation>
                     <div className="text-2xl text-slate-800 md:absolute md:left-[10%] md:top-[50%] md:-translate-y-[50%] lg:top-[55%] lg:-translate-y-[55%] lg:text-[38px] lg:leading-[42px]">
                         <ul className="flex flex-wrap items-center justify-center gap-x-[75px] gap-y-8 text-[88px] leading-[92px]">
                             {contacts.map((contact) => {
