b47cb58ecfae97a65ef7230fb3737e7a71eb294a
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 04:30:12 +0530

    Update components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Layout } from './Layout/Layout'
 export { default as ProjectCard } from './ProjectCard/ProjectCard'
+export { default as SlideInAnimation } from './SlideInAnimation/SlideInAnimation'
