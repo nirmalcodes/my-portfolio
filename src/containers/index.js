c623b755b28b1ac50a877e5f8b0deb0b1f4962e9
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 00:50:30 +0530

    Update containers index

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as HeroContainer } from './HeroContainer/HeroContainer'
 export { default as AboutContainer } from './AboutContainer/AboutContainer'
 export { default as ProjectsContainer } from './ProjectsContainer/ProjectsContainer'
+export { default as ContactContainer } from './ContactContainer/ContactContainer'
