cc52556fa6ed79a344964c55b6a822fdc7b5bc12
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 00:49:10 +0530

    Update AboutContainer

--- src/containers/AboutContainer/AboutContainer.jsx
+++ src/containers/AboutContainer/AboutContainer.jsx
@@ -4,8 +4,8 @@ const AboutContainer = () => {
     return (
         <>
             <section className="section" id={'about'}>
-                <div className="container flex min-h-screen flex-col items-center justify-center p-4 md:block">
-                    <h3 className="relative mb-7 text-[64px] font-bold leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[160px] xl:text-[200px]">
+                <div className="container flex min-h-screen flex-col items-center justify-center md:block md:p-4">
+                    <h3 className="relative mb-7 text-[64px] font-bold uppercase leading-none text-slate-800 opacity-10 md:absolute md:top-[40%] md:-translate-y-[40%] md:text-[128px] lg:top-[45%] lg:-translate-y-[45%] lg:text-[160px] xl:text-[200px]">
                         About Me
                     </h3>
                     <p className="text-2xl text-slate-800 md:absolute md:left-[15%] md:top-[55%] md:-translate-y-[55%] lg:top-[65%] lg:-translate-y-[65%] lg:text-[38px] lg:leading-[42px]">
