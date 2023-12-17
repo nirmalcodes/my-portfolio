696ee990ce093bf66571d3fcfe12873495fcd331
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-17 18:41:28 +0530

    Update components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Layout } from './Layout/Layout'
 export { default as ProjectCard } from './ProjectCard/ProjectCard'
 export { default as SlideInAnimation } from './SlideInAnimation/SlideInAnimation'
+export { default as IconSelector } from './IconSelector/IconSelector'
