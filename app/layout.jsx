94d8aefc9f83a007d487fd0b041ecd8e72024ccd
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-03-11 05:07:48 +0530

    Add speed-insights to the project

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/shared/Navbar';
 import STTButton from '@/components/STTButton';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <STTButton />
+        <SpeedInsights />
       </body>
     </html>
   );
